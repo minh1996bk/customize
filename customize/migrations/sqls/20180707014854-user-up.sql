9e0d3946270bce7c8ed486257cde4250061c688f
minh1996bk <minh1996bk@gmail.com>
2018-07-07 09:36:44 +0700

    [add] user migration

---
+++ customize/migrations/sqls/20180707014854-user-up.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(63) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) 
+)
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+AVG_ROW_LENGTH = 0
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+KEY_BLOCK_SIZE = 0
+MAX_ROWS = 0
+MIN_ROWS = 0
+ROW_FORMAT = Compact;
